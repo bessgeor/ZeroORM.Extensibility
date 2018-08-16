dc29cfce916a152091ad5874576230f20844eb68
bessgeor <bessgeor@gmail.com>
2018-08-16 17:09:24 +0300

    Add expression parsing exception for simple property access

---
+++ Common/Expressions/Exceptions/ExpressionsException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ZeroORM.Extensibility.Common.Expressions.Exceptions
+{
+	public class ExpressionsException : Exception
+	{
+		public ExpressionsException() { }
+		public ExpressionsException(string message) : base(message) { }
+		public ExpressionsException(string message, Exception inner) : base(message, inner) { }
+	}
+}
---
+++ Common/Expressions/Exceptions/ShouldBeSimplePropertyAccessException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ZeroORM.Extensibility.Common.Expressions.Exceptions
+{
+	public class ShouldBeSimplePropertyAccessException : ExpressionsException
+	{
+		public ShouldBeSimplePropertyAccessException( Expression targetExpression )
+			: base( $@"Passed expression should be simple property access of the first order, like ""param => param.Property"", but ""{targetExpression}"" was passed in" )
+		{
+
+		}
+	}
+}
